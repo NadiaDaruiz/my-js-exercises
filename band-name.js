a6abd19b4d93e94fb3f99db87d6c970c67ad4679
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-09-03 23:42:59 +0200

    Both exercises

--- band-name.js
+++ band-name.js
@@ -1,3 +1,17 @@
+// # Band name
+
+// function bandName(clothesColor, foodEaten) {
+//   const colorInitialInCaps =
+//     clothesColor[0].toUpperCase() + clothesColor.substring(1).toLowerCase();
+//   const foodInitialInCaps =
+//     foodEaten[0].toUpperCase() + foodEaten.substring(1).toLowerCase();
+
+//   const generateBandName = `The ${colorInitialInCaps} ${foodInitialInCaps}!`;
+
+//   return bandName;
+// }
+// console.log(bandName("brown", "cake"));
+
 // # Refactor band name
 
 const bandArray = [];
