8e2a26ec5c9b03740967d28f72d3ec2b62f0ffb3
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-09-10 18:42:42 +0200

    Add calling two functions and one function call twice

--- band-name.js
+++ band-name.js
@@ -1,32 +1,78 @@
-// # Band name
+// # 1. Band name
 
 // function bandName(clothesColor, foodEaten) {
-//   const colorInitialInCaps =
+
+//   let color =
 //     clothesColor[0].toUpperCase() + clothesColor.substring(1).toLowerCase();
-//   const foodInitialInCaps =
+//   let food =
 //     foodEaten[0].toUpperCase() + foodEaten.substring(1).toLowerCase();
 
-//   const generateBandName = `The ${colorInitialInCaps} ${foodInitialInCaps}!`;
-
-//   return bandName;
+//   return `The ${color} ${food}!`;
 // }
 // console.log(bandName("brown", "cake"));
 
-// # Refactor band name
+// # 2. Refactor band name with global array
+
+// const bandArray = [];
+
+// function bandName(clothesColor, foodEaten) {
+//   let newBandName = "";
+
+//   let color =
+//     clothesColor[0].toUpperCase() + clothesColor.substring(1).toLowerCase();
+//   let food = foodEaten[0].toUpperCase() + foodEaten.substring(1).toLowerCase();
+
+//   newBandName = `The ${color} ${food}!`;
+
+//   bandArray.push(newBandName);
+
+//   return newBandName;
+// }
+
+// console.log(bandName("brown", "cake"));
+// bandName("blue", "paprika");
+// console.log(bandArray);
+
+// # 3. Calling two other functions --
 
 const bandArray = [];
 
-function bandName(clothesColor, foodEaten) {
-  const colorInitialInCaps =
-    clothesColor[0].toUpperCase() + clothesColor.substring(1).toLowerCase();
-  const foodInitialInCaps =
-    foodEaten[0].toUpperCase() + foodEaten.substring(1).toLowerCase();
+// function bandName(clothesColor, foodEaten) {
+//   const color = capitalizeColor(clothesColor);
+//   const food = capitalizeFood(foodEaten);
 
-  const generateBandName = `The ${colorInitialInCaps} ${foodInitialInCaps}!`;
+//   let newBandName = `The ${color} ${food}!`;
 
-  bandArray.push(generateBandName);
+//   bandArray.push(newBandName);
 
-  return bandArray;
+//   return newBandName;
+// }
+
+// function capitalizeColor(clothesColor) {
+//   return clothesColor[0].toUpperCase() + clothesColor.substring(1).toLowerCase()}
+
+// function capitalizeFood(foodEaten) {
+//   return foodEaten[0].toUpperCase() + foodEaten.substring(1).toLowerCase();
+// }
+
+// # 4. A single function call twice
+
+function bandName(clothingColor, foodEaten) {
+  let band = `The ${capitalizeInitial(clothingColor)} ${capitalizeInitial(
+    foodEaten
+  )}!`;
+
+  bandArray.push(band);
+
+  return band;
+}
+
+function capitalizeInitial(rawString) {
+  return rawString[0].toUpperCase() + rawString.substring(1).toLowerCase();
 }
 
 console.log(bandName("brown", "cake"));
+bandName("blue", "paprika");
+console.log(bandArray);
+
+// # 5. MakeTasty with switch statements
