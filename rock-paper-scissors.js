eae56455a2b0e18161ada950aed6defe7eeb0d24
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-10-16 21:52:37 +0200

    Add modif to ex

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,5 @@
-// # 1. The unbeatable
-/*
+// # 1. The Unbeatable
+
 const unbeatableRockPaperScissors = userMove => {
   let aiMove = "";
   const cleanUserMove = userMove.toString().toLowerCase();
@@ -28,13 +28,12 @@ console.log(unbeatableRockPaperScissors("ROck"));
 console.log("\nWith 'scissors' as an argument:");
 console.log(unbeatableRockPaperScissors("scissors"));
 console.log(unbeatableRockPaperScissors(1));
-*/
 
-// #2.The Honest
+// #2. The Honest
 
 const honestRockPaperScissor = userMove => {
   let moves = ["paper", "rock", "scissor"];
-  let aiMove = randomPicker(moves);
+  let aiMove = randomPickerOne(moves);
 
   const cleanUserMove = userMove.toString().toLowerCase();
 
@@ -44,7 +43,7 @@ const honestRockPaperScissor = userMove => {
   return aiMove;
 };
 
-const randomPicker = moves => {
+const randomPickerOne = moves => {
   const index = Math.floor(Math.random() * moves.length);
   return moves[index];
 };
@@ -57,3 +56,64 @@ console.log(honestRockPaperScissor("ROck"));
 console.log("\nWith 'scissors' as an argument:");
 console.log(honestRockPaperScissor("scissors"));
 console.log(honestRockPaperScissor(0));
+
+// #3. Two Players
+
+const rspWithTwoPlayers = () => {};
+
+// #3a. Two Players (my own version using random for defining the moves) ASK FOR HELP!
+
+/* const rpsTwoPlayers = () => {
+  let moves = ["paper", "rock", "scissor"];
+
+  let playerA = randomPickerOne(moves);
+  let playerB = randomPickerTwo(moves);
+
+  let result = `Player A plays ${playerA} and Player B plays ${playerB}.`;
+
+  return result;
+};
+
+const randomPickerTwo = moves => {
+  const index = Math.floor(Math.random() * moves.length);
+  return moves[index];
+};
+
+const theWinner = () => {
+let thePlayerA = "";
+let thePlayerB = "";
+
+  if (thePlayerA == "paper") {
+    switch (thePlayerB) {
+      case "paper":
+        return `It's a draw!`;
+        break;
+      case "scissor":
+        return `Player B wins!`;
+        break;
+      case "rock":
+        return `Player A wins!`;
+        break 
+    } 
+    else if(thePlayerA == "rock"){
+        switch (thePlayerB){
+         case "paper":
+            return `Player B wins!`;    
+         case "rock": 
+            return `It's a draw!`;   
+         case "scissor":
+            return `Player A wins!`;    
+        }
+         else 
+
+    }
+
+} 
+};
+
+console.log("\nWith two players:");
+rpsTwoPlayers();
+
+//theWinner();
+
+*/
