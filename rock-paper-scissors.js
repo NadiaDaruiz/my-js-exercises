11ac2a10e52e61ea45ac1d35ad2cbeb865ec3fae
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-10-18 15:52:00 +0200

    Two players and random

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -20,7 +20,7 @@ const unbeatableRockPaperScissors = userMove => {
   return aiMove;
 };
 
-console.log("\nThe Unbeatable:");
+console.log("\tThe Unbeatable:");
 console.log("\nWith 'paper' as an argument:");
 console.log(unbeatableRockPaperScissors("paper"));
 console.log("\nWith 'rock' as an argument:");
@@ -48,7 +48,7 @@ const randomPickerOne = moves => {
   return moves[index];
 };
 
-console.log("\nThe Honest:");
+console.log("\tThe Honest:");
 console.log("\nWith 'paper' as an argument:");
 console.log(honestRockPaperScissor("paper"));
 console.log("\nWith 'rock' as an argument:");
@@ -59,61 +59,71 @@ console.log(honestRockPaperScissor(0));
 
 // #3. Two Players
 
-const rspWithTwoPlayers = () => {};
+const rpsWithTwoPlayers = (playerA, playerB) => {
+  let result = "";
 
-// #3a. Two Players (my own version using random for defining the moves) ASK FOR HELP!
+  console.log(`Player A plays ${playerA} and Player B plays ${playerB}`);
 
-/* const rpsTwoPlayers = () => {
-  let moves = ["paper", "rock", "scissor"];
-
-  let playerA = randomPickerOne(moves);
-  let playerB = randomPickerTwo(moves);
-
-  let result = `Player A plays ${playerA} and Player B plays ${playerB}.`;
-
+  if (playerA == "paper" && playerB == "scissors") {
+    result = `Player B is the winner!`;
+  } else if (playerA == "rock" && playerB == "scissors") {
+    result = `Player A is the winner!`;
+  } else if (playerA == "scissors" && playerB == "paper") {
+    result = `Player A is the winner!`;
+  } else if (playerA == "rock" && playerB == "paper") {
+    result = `Player B is the winner!`;
+  } else if (playerA == "paper" && playerB == "rock") {
+    result = `Player A is the winner!`;
+  } else if (playerA == "scissors" && playerB == "rock") {
+    result = `Player B is the winner!`;
+  } else if (playerA === playerB) {
+    result = `It's a draw!`;
+  }
   return result;
 };
+console.log(`\tWith Two Players:`);
+console.log(rpsWithTwoPlayers("paper", "paper"));
+console.log(rpsWithTwoPlayers("rock", "scissors"));
+console.log(rpsWithTwoPlayers("scissors", "paper"));
+console.log(rpsWithTwoPlayers("rock", "paper"));
 
-const randomPickerTwo = moves => {
-  const index = Math.floor(Math.random() * moves.length);
-  return moves[index];
+// #3a. Two Players (my own version using random for defining the moves and later check who won)
+
+const options = ["paper", "rock", "scissors"];
+
+const moves = () => {
+  const index = Math.floor(Math.random() * options.length);
+  return options[index];
 };
 
-const theWinner = () => {
-let thePlayerA = "";
-let thePlayerB = "";
+const checkWinner = (playerA, playerB) => {
+  if (playerA == playerB) {
+    return `It's a draw!`;
+  }
 
-  if (thePlayerA == "paper") {
-    switch (thePlayerB) {
-      case "paper":
-        return `It's a draw!`;
-        break;
-      case "scissor":
-        return `Player B wins!`;
-        break;
-      case "rock":
-        return `Player A wins!`;
-        break 
-    } 
-    else if(thePlayerA == "rock"){
-        switch (thePlayerB){
-         case "paper":
-            return `Player B wins!`;    
-         case "rock": 
-            return `It's a draw!`;   
-         case "scissor":
-            return `Player A wins!`;    
-        }
-         else 
+  let winnerTable = [
+    ["rock", "scissors", 0],
+    ["paper", "scissors", 1],
+    ["scissors", "paper", 0],
+    ["scissors", "rock", 1],
+    ["rock", "paper", 1],
+    ["paper", "rock", 0]
+  ];
 
+  let winner;
+  winnerTable.forEach(tableEntry => {
+    if (tableEntry[0] == playerA && tableEntry[1] == playerB) {
+      let winnerIndex = tableEntry[2];
+      winner = tableEntry[winnerIndex];
     }
-
-} 
+  });
+  return `And the winner is ${winner}`;
 };
 
-console.log("\nWith two players:");
-rpsTwoPlayers();
+const playerA = moves();
+const playerB = moves();
 
-//theWinner();
-
-*/
+console.log(`\tWith Two Players Random:`);
+moves();
+console.log(`Player A plays ${playerA} and Player B plays ${playerB}.`);
+console.log(checkWinner(playerA, playerB));
